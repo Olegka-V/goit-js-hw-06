99e9aaa0eb0990c04c9bbaa4671c42a216a382de
Trtl <47746034+Olegka-V@users.noreply.github.com>
2024-02-24 23:29:59 +0200

    finish hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,32 @@
-function getElementWidth(content, padding, border) {
-    // 1. Всі елементи цифрами
-    const parseContent = Number.parseFloat(content);
-    const parsePadding = Number.parseFloat(padding);
-    const parseBorder =  Number.parseFloat(border);
-    // 2. Перемноження аргументів
-    const result = parseContent + parsePadding * 2 + parseBorder * 2;
-    // 3. Повернути значення
-    return result;
+class StringBuilder { 
+    #value; 
 
-    
+    constructor(value) {
+        this.#value = value;
+     }
+
+    getValue() { 
+        return this.#value;
+    }
+
+    padEnd(str) { 
+        this.#value = this.#value + str;
+    }
+
+    padStart(str) { 
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) { 
+        this.#value = str + this.#value + str;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
